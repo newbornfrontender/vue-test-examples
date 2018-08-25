18867e3c0a194b8e121ac36fddbcad64ef2f3057
Infant Frontender <newbornfrontender@gmail.com>
2018-08-25 19:39:25 +0300

    Добавил основной файл

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <section id="app">
+    <Header/>
+    <Main/>
+  </section>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main,
+  },
+};
+</script>
