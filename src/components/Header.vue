0937446b7177bd50797c8802cc23532f56b4fab1
Infant Frontender <newbornfrontender@gmail.com>
2018-08-25 19:52:26 +0300

    Добавил компоненты

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li
+          v-for="item in list"
+          v-bind:key="item.id"
+        >
+          <router-link v-bind:to="item.url">{{ item.text }}</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      list: [{
+        url: '/',
+        text: 'Home',
+      }, {
+        url: '/dynamic-attributes',
+        text: 'Dynamic attributes',
+      }],
+    };
+  },
+};
+</script>
