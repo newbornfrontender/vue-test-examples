668f69a742dd964dcca2d3ae936e622b23ea42a5
Infant Frontender <newbornfrontender@gmail.com>
2018-08-26 17:19:00 +0300

    Убрал элементы переопределения имен классов, т.к. стили применялиь ко всем элементам

--- src/router.js
+++ src/router.js
@@ -29,8 +29,6 @@ function title(sub) {
 export default new Router({
   mode: 'history',
   base: '/',
-  linkActiveClass: 'routerActive',
-  linkExactActiveClass: 'routerActiveExact',
   routes: [
     {
       path: '/',
