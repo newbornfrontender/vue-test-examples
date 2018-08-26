efca91522672b2f2d281a111e08e6244de6c717e
Infant Frontender <newbornfrontender@gmail.com>
2018-08-26 11:30:53 +0300

    Протестировал media queries и добавил пример

---
+++ src/views/MediaQueries.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <p>Media queries</p>
+    <p>{{ window.width }}</p>
+    <p v-if="bp.max.md">Hidden text</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MediaQueries',
+  data () {
+    return {
+      window: {
+        width: 0,
+      },
+    };
+  },
+  created () {
+    window.addEventListener('resize', this.getClientWidth);
+    this.getClientWidth();
+  },
+  destroyed () {
+    window.removeEventListener('resize', this.getClientWidth);
+  },
+  methods: {
+    getClientWidth () {
+      this.window.width = document.documentElement.clientWidth;
+    },
+  },
+};
+</script>
