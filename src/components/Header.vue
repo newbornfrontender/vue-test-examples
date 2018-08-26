50c5e07ece112bff9e94c734c59adb8badc47956
Infant Frontender <newbornfrontender@gmail.com>
2018-08-26 11:45:15 +0300

    Настроил вывод ссылок по порядку

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <nav>
       <ul>
         <li
-          v-for="item in list"
+          v-for="item in orderedList"
           v-bind:key="item.id"
         >
           <router-link v-bind:to="item.url">{{ item.text }}</router-link>
@@ -14,21 +14,31 @@
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   name: 'Header',
   data () {
     return {
       list: [{
+        order: '1',
         url: '/',
         text: 'Home',
       }, {
+        order: '3',
         url: '/dynamic-attributes',
         text: 'Dynamic attributes',
       }, {
+        order: '2',
         url: '/media-queries',
         text: 'Media queries',
       }],
     };
+  },
+  computed: {
+    orderedList () {
+      return _.orderBy(this.list, 'order');
+    },
   },
 };
 </script>
